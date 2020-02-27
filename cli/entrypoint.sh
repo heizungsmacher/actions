ce3c43a8514be27bdd22fa22ff8dd443650c910e
Robin Mehner <robin@coding-robin.de>
2020-02-27 11:01:28 +0100

    Adapt changes from @IvanJosipovic's fork to have output variable
    https://github.com/IvanJosipovic/actions/commit/296bf61998720bbe96bbf32fd441b5fba4e8535b

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -3,5 +3,5 @@
 if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
   echo "$BUILD_DIR didn't change"
 else
-  ${BUILD_COMMAND:-echo} && netlify $*
+  ${BUILD_COMMAND:-echo} && output=$(netlify $*); echo ::set-output name=output::$output;
 fi
